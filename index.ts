6f750c9ba2d8d84cbfb973014ec31d2a7b289c90
Carlos Ramos <carlosefrfloresta@gmail.com>
2020-03-12 21:14:34 -0300

    Added types script dependency

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
+import {TransformationContext,Transformer,visitNode, SourceFile,Visitor, Node, VisitResult, visitEachChild} from 'typescript';
+
 class Test {
     public call(): void {
         console.log('AAAAAAAAAAAAAAAAAAAA   AA')
     }
 }
+
+function tranformer(/*opt?: Opts*/) {
+    function visitor(context: TransformationContext, sourceFile: SourceFile ) {
+        const visitor: Visitor = (node: Node): VisitResult<any> => {
+            return visitEachChild(node, visitor, context);
+        }
+        return visitor;
+    }
+    return (context: TransformationContext) : Transformer<any> => {
+        return (source: SourceFile) => visitNode(source, visitor(context, source))
+    }
+}
+
 const f = 'sdd';
 
 console.log('teste  2')
